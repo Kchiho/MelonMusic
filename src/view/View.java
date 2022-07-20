d46e2c0d98ffc94b15687ec5062eca6c76248022
Kchiho <chiho1998@naver.com>
2022-07-20 14:03:31 +0900

    2.4v

--- src/view/View.java
+++ src/view/View.java
@@ -181,7 +181,7 @@ public class View {
 		}
 		System.out.println();
 	}
-	
+
 	public void searchMusicPage() {
 		System.out.println("노래 검색 메뉴입니다.");
 	}
@@ -221,11 +221,11 @@ public class View {
 		System.out.println("노래 추천 메뉴입니다.");
 	}
 	public void musicPlayer() {
-        System.out.println("1:00  ─|──────────── 5:57");
-        System.out.println("   ⇄   ◁◁   II   ▷▷   ↻   ");
-        System.out.println("Volume: ■ ■ ■ ■ ■ □ □ □");
-        System.out.println();
-    }
+		System.out.println("1:00  ─|──────────── 5:57");
+		System.out.println("   ⇄   ◁◁   II   ▷▷   ↻   ");
+		System.out.println("Volume: ■ ■ ■ ■ ■ □ □ □");
+		System.out.println();
+	}
 	public void musicInfo(MMusicVO vo) {
 		System.out.println(vo.getmTitle()+"\n"
 				+vo.getmArtist()+"\t\t"+"♬:"+vo.getmHit());
